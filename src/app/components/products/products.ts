9a26691d9ee22bb07bb430ed011882f0afc28d1d
teodora <teddy.georgieva97@gmail.com>
2025-12-09 22:28:25 +0200

    feat(components): add products and transaction-panel components

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -1,32 +1,106 @@
-import { Component } from '@angular/core';
-import {ProductService} from '../../service/productService';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {Component, inject} from '@angular/core';
 
-interface ProductProps {
-  id: string;
-  name: string;
-  price: number;
-  quantity: number;
-}
+import {ProductService} from '../../services/api/product.service';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectChange, MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {Observable} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
+import {PriceService} from '../../services/api/price.service';
+import {MatButton} from '@angular/material/button';
+import {ConfirmationDialog} from '../confirmation-dialog/confirmation-dialog';
+import {MatDialog} from '@angular/material/dialog';
+import {CurrencyBGNDirective} from '../../directives/currency-bgn.directive';
 
 @Component({
   selector: 'app-products',
   imports: [
-    MatFormFieldModule, MatSelectModule, ReactiveFormsModule, MatInputModule
+    MatFormFieldModule, MatSelectModule, ReactiveFormsModule, MatInputModule, AsyncPipe, FormsModule, MatButton, CurrencyBGNDirective
   ],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
+
 export class Products {
-  productsControl = new FormControl<ProductProps | null>(null);
-  products: ProductProps[] = [];
-  constructor(private productService: ProductService) {
-     productService.getProducts().subscribe((product)=>{
-       this.products = product;
-      console.log(product)
-    })
+  productForm: FormGroup;
+  productService = inject(ProductService)
+  products: Observable<Product[]> = this.productService.getProducts();
+
+  constructor(private priceService: PriceService, private fb: FormBuilder, private dialog: MatDialog
+  ) {
+    this.productForm = this.fb.group({
+      product: ['', Validators.required],
+      coins: [
+        '',
+        [
+          Validators.required,
+          Validators.min(0.01)
+        ]
+      ]
+    });
+
+    this.productForm.get('coins')!.valueChanges.subscribe(value => {
+      if (typeof value === 'number') {
+        this.priceService.setCoins(value);
+      }
+    });
   }
+
+  setPrice(event: MatSelectChange) {
+    this.priceService.setPrice(event.value.price);
+  }
+
+  cancelOrder() {
+    const confirmDialog = this.dialog.open(ConfirmationDialog, {
+      data: {
+        title: 'Cancel your order',
+        message: 'Are you sure you want to cancel your order?'
+      }
+    });
+    confirmDialog.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.resetForm();
+      }
+    });
+  }
+
+  confirmOrder() {
+    if (this.productForm.invalid) return;
+
+    const { product, coins } = this.productForm.getRawValue();
+    const changeAmount = coins - product.price;
+
+    const dialog = this.dialog.open(ConfirmationDialog, {
+      data: {
+        title: 'Confirm order',
+        message: `You will purchase ${product.name}. Your change is ${changeAmount} BGN.`
+      }
+    });
+
+    dialog.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.resetForm();
+      }
+    });
+  }
+
+  resetForm() {
+    this.priceService.setPrice(0);
+    this.priceService.setCoins(0);
+
+    this.productForm.reset({
+      product: '',
+      coins: ''
+    });
+
+    Object.keys(this.productForm.controls).forEach(key => {
+      const control = this.productForm.get(key);
+      control?.setErrors(null);
+      control?.markAsPristine();
+      control?.markAsUntouched();
+    });
+  }
+
 }
+
--- src/app/components/transaction-panel/transaction-panel.ts
+++ src/app/components/transaction-panel/transaction-panel.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {PriceService} from '../../services/api/price.service';
+import {AsyncPipe, CurrencyPipe} from '@angular/common';
+import {combineLatest, map} from 'rxjs'
 
 @Component({
   selector: 'app-transaction-panel',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    CurrencyPipe,
+  ],
   templateUrl: './transaction-panel.html',
   styleUrl: './transaction-panel.css',
 })
 export class TransactionPanel {
-
+  priceService = inject(PriceService)
+  productPrice$ = this.priceService.price$;
+  insertedAmount$ = this.priceService.coins$;
+  changeAmount$ = combineLatest([
+    this.productPrice$,
+    this.insertedAmount$
+  ]).pipe(
+    map(([price, coins]) => coins - price)
+  );
 }
