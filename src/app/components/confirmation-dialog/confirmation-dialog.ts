8405c7ba96d5b3482a8fb04d5472293aeb120dc9
teodora <teddy.georgieva97@gmail.com>
2025-12-09 22:33:17 +0200

    feat(components): add dialog component

--- src/app/components/confirmation-dialog/confirmation-dialog.ts
+++ src/app/components/confirmation-dialog/confirmation-dialog.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from '@angular/material/dialog';
+import {MatButton} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-confirmation-dialog',
-  imports: [],
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogClose,
+    MatDialogActions,
+    MatButton
+  ],
   templateUrl: './confirmation-dialog.html',
   styleUrl: './confirmation-dialog.css',
 })
 export class ConfirmationDialog {
+  readonly dialogRef = inject(MatDialogRef<ConfirmationDialog>);
+  readonly data = inject<{ title: string, message: string }>(MAT_DIALOG_DATA);
 
+  onClose(): void {
+    this.dialogRef.close();
+  }
 }
